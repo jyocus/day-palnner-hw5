cc1c46c897535fcfc2e04d45ceafaea1936ac634
Justin Yocus <justin.o.yocus@gmail.com>
2020-10-04 19:15:04 -0500

    pseudo code and planning completed

---
+++ script.js
@@ -0,0 +1,7 @@
+// Psuedo Code 
+// - First create time blocks in HTMLAllCollection, can leverage bootstrap for easier formatting 
+// - Add current date, can use moment.js for formatting 
+// Variables declared for current date, individual time blocks, variables to use exisiting css classes and ids
+// Can potentially add classes for past, present and future times and then build a function to add those classes for color change
+// Need to add local storage so the page saves tasks upon refresh - use localStorage.getItem 
+
